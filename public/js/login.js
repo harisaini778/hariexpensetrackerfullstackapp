bbad98596a29361d6651e826d3b787f1b7a95e2e
harisaini778 <harikumarsaini778@gmail.com>
2024-02-10 12:32:47 +0530

    updated the login/signup page ui

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,20 @@
-const signUp = document.getElementById("signup");
-const signIn = document.getElementById("signIn");
-const container = document.getElementById("container");
-const signUpBtn = document.getElementById("signUpBtn");
-const signInBtn = document.getElementById("signInBtn");
+function toggleForm() {
+    var loginForm = document.getElementById("login-form");
+    var signupForm = document.getElementById("signup-form");
 
-signUp.addEventListener("click",()=>{
-    container.classList.add("right-panel-active")
+    if (loginForm.style.display === "none") {
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
+    } else {
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
+    }
+}
+
+document.getElementById("login-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+});
+
+document.getElementById("signup-form").addEventListener("submit", function (event) {
+    event.preventDefault();
 });
